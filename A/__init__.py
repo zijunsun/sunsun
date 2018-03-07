77008a3b43ae24dd02577ba88c5b596eac9377bf
zijunsun <stefan_sun_cn@hotmail.com>
2018-03-07 17:56:06 +0800

    ddF

---
+++ A/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@file  : __init__.py.py
+@author: zijun
+@contact : stefan_sun_cn@hotmail.com
+@date  : 2018/3/7 17:54
+@version: 1.0
+@desc  : 
+"""
